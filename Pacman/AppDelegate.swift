e0b1eb7b6705a1eda9b428b553344bc39c2e5c0e
Padraig Scott <padraig@padraigs-mbp.home>
2016-11-12 16:24:49 +0000

    Initial Commit

---
+++ Pacman/AppDelegate.swift
@@ -0,0 +1,25 @@
+//
+//  AppDelegate.swift
+//  Pacman
+//
+//  Created by Padraig Scott on 12/11/2016.
+//  Copyright © 2016 Padraig Scott. All rights reserved.
+//
+
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+    
+    
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+    
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+    
+    
+}
